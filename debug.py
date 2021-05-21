5e7e7d682f5e1600e34c0f969650690ac56dc851
Sturz888 <rlsturza@student.rtc.edu>
2021-05-21 15:12:05 -0700

    Update debug.py
    added inputs for a,b, and c with a print lone_sum that points to the rest of the code. also moved a>=b above a==b a==c b==c so that it can be ran

--- debug.py
+++ debug.py
@@ -1,11 +1,18 @@
 def lone_sum(a, b, c):
-    if a >= b:
+      
+    if a == b and a == c and b == c:
         return c
     elif a == c:
         return b
     elif b == c:
         return a
-    elif a == b and a == c and b == c:
+    elif a >= b:
         return 0
     else:
-        return a+b+c
+        return a + b + c 
+
+a=int(input("Input A :"))
+b=int(input("Input B :"))
+c=int(input("Input C :"))
+
+print(lone_sum(a,b,c) )
